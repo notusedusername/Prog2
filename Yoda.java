29c3670ddbeba20934b4fad4b439f4166096a608
notusedusername <36472592+notusedusername@users.noreply.github.com>
2018-09-16 21:12:44 +0200

    Create Yoda.java

---
+++ Yoda.java
@@ -0,0 +1,17 @@
+public class Yoda {
+
+    public static void main(String[] args) {
+      String textext = null;
+      if ("almalé".equals(textext)) {
+        /* ... */ 
+      }
+      System.out.println("almalé".equals(textext));
+      
+      textext = null;
+      if (textext.equals("aa")) {
+        /*...*/
+      }
+      System.out.println("aa".equals(textext));
+    }
+    
+}
