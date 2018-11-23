ab789020a9c232d667a131db84951de5c83b2497
notusedusername <36472592+notusedusername@users.noreply.github.com>
2018-11-23 19:19:10 +0100

    Update Gagyi.java

--- Gagyi.java
+++ Gagyi.java
@@ -18,24 +18,26 @@ public class Gagyi {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException {
-       Integer x = 127;
-       Integer t = 127;
+       Integer x = 127; //Integer.valueOf(127)
+       Integer t = 127; //Integer.valueOf(127) ugyanarra hivatkozik
        
-       System.out.println(x);
-       System.out.println(t);
+       System.out.println(x == t); //emiatt biztosan nem lép be az 1 whileba
+
        
        while (x <= t && x >= t && t != x){
-           System.out.println(1);
+           System.out.println(1); 
            //nem lép be a ciklusba
        }
        
        x = 128;
        t = 128;
        
+       System.out.println(x == t); //a két hivatkozás már nem egyezik meg
+       
        while (x <= t && x >= t && t != x){
-           System.out.println("VÉGTELEEEEN");
-           sleep(3000);
-           //belép a ciklusba
+           System.out.println("VÉGTELEEEEN");   //belép, mert a 128 már nincs 
+           sleep(3000);                 // poolozva, igy a két értéknek 2 külön címe lesz,
+                                        // azaz nem egyenlőek
        }
     
     }
